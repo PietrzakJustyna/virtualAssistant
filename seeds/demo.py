326dc8afd499368593c0a33f4c563885030afb47
Justyna Pietrzak <tynk713@gmail.com>
2020-05-01 15:15:41 +0200

    Some fixes

--- seeds/demo.py
+++ seeds/demo.py
@@ -1,7 +1,42 @@
 from flask_seeder import Seeder, Faker, generator
+from flask_seeder.generator import Generator
 import sys
-sys.path.append("..")
 from virtualassistant.models import Assistant
+from urllib.request import urlopen
+import json
+
+def get_jsonparsed_data(url):
+    lines = []
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    json_dict = json.loads(data)
+
+    for elem in json_dict:
+        if elem.get("title") is not None:
+            lines.append(elem.get("title"))
+
+    return lines
+
+class Job(Generator):
+    """ Job generator from json list"""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._lines = None
+
+    def generate(self):
+        """ Generate a random job title from the list
+
+        Returns:
+            A random job title in string format
+        """
+        if self._lines is None:
+            self._lines = get_jsonparsed_data("http://api.dataatwork.org/v1/jobs")
+
+        result = self.rnd.choice(self._lines)
+
+        return result
+
 
 # All seeders inherit from Seeder
 class DemoSeeder(Seeder):
@@ -14,7 +49,7 @@ class DemoSeeder(Seeder):
         "id": generator.Sequence(),
         "name": generator.Name(),
         "surname": generator.Name(),
-        "job": generator.Job(),
+        "job": Job(),
         "photo_path": "./static/uploads/default.jpg"
       }
     )
--- virtualassistant/config.py
+++ virtualassistant/config.py
@@ -4,7 +4,7 @@ import os
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "virtualassistant.db"))
 
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = 'virtualassistant/static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 SQLALCHEMY_DATABASE_URI = database_file
